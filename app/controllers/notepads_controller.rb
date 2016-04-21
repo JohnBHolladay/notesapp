7148792d3a50d5482fb9a3cb7fde2c54750a9cfc
JohnBHolladay <JohnBHolladay@gmail.com>
2016-04-21 07:13:22 -0500

    Added CSS Styling for the main page and the notepad viewing page. Used placeholder text for the creation text field to make it easier to use a custom identifier.

--- app/controllers/notepads_controller.rb
+++ app/controllers/notepads_controller.rb
@@ -1,20 +1,25 @@
 class NotepadsController < ApplicationController
   def new
-    @notepad = Notepad.new({url: SecureRandom.uuid})
+    #Generate a random url identifier. Later, this should create a more human friendly result. 
+    @notepad = Notepad.new({url: SecureRandom.uuid.upcase!})
   end
 
   def create
+    
+    #Check if the entry was blank, if so use the placeholder url. 
+    if params[:notepad][:url] == ""
+      params[:notepad][:url] = params[:notepad][:orig_url]
+    end
+
+    #Save the new notepad, then redirect to it. If the notepad already exists, this will redirect to the existing notepad. 
     @notepad = Notepad.new(notepad_params)
     @notepad.save
 
-    #if @notepad.save
-      redirect_to notepad_path @notepad.url
-    #else
-    #  redirect_to notepad_path @notepad.url
-    #end
+    redirect_to notepad_path @notepad.url
   end
 
   def show
+    #Find the notepad by the url identifier, rather than ID. 
     @notepad = Notepad.find_by(url: params[:id])
   end
 
