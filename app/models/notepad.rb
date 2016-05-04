e2d0a9db28749d094427646f4f316a6f2773a12d
JohnBHolladay <JohnBHolladay@gmail.com>
2016-05-04 05:29:08 -0500

    Added art for notes, and updated stylesheet to be more vibrant.

--- app/models/notepad.rb
+++ app/models/notepad.rb
@@ -1,9 +1,13 @@
 class Notepad < ActiveRecord::Base
-  has_many :notes, dependent: :destroy
+  has_many :notes, -> {order 'created_at asc'}, dependent: :destroy
   validates :url, presence: true, uniqueness: true
 
   #The url identifier should always be uppercase. 
   def url=(s)
     write_attribute(:url, s.to_s.upcase)
+  end
+
+  def children
+    @children ||= (self.notes.all).sort(&:created_at)
   end
 end
